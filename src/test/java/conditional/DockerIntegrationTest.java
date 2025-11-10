8edc5fa158c230d10d117477373aca1680e70fad
Shukhrat test <shukhrat.abdullayev@stoloto.ru>
2025-11-10 14:27:04 +0300

    src/test/java/conditional/ DevProfileTest.java          # Unit-тест dev профиля ProdProfileTest.java         # Unit-тест prod профиля ProfileControllerTest.java   # Integration-тест контроллера DockerIntegrationTest.java   # Docker integration-тест

--- src/test/java/conditional/DockerIntegrationTest.java
+++ src/test/java/conditional/DockerIntegrationTest.java
@@ -30,13 +30,10 @@ class DockerIntegrationTest {
         Integer devMappedPort = devApp.getMappedPort(8080);
         String devUrl = "http://localhost:" + devMappedPort + "/profile";
 
-        System.out.println("Testing Dev container at: " + devUrl);
-
         ResponseEntity<String> devResponse = restTemplate.getForEntity(devUrl, String.class);
 
         assertEquals(200, devResponse.getStatusCodeValue());
         assertEquals("Current profile is dev", devResponse.getBody());
-        System.out.println("Dev Response: " + devResponse.getBody());
     }
 
     @Test
@@ -45,23 +42,9 @@ class DockerIntegrationTest {
         Integer prodMappedPort = prodApp.getMappedPort(8081);
         String prodUrl = "http://localhost:" + prodMappedPort + "/profile";
 
-        System.out.println("Testing Prod container at: " + prodUrl);
-
         ResponseEntity<String> prodResponse = restTemplate.getForEntity(prodUrl, String.class);
 
         assertEquals(200, prodResponse.getStatusCodeValue());
         assertEquals("Current profile is production", prodResponse.getBody());
-        System.out.println("Prod Response: " + prodResponse.getBody());
-    }
-
-    @Test
-    void testContainersAreRunning() {
-        assertTrue(devApp.isRunning(), "Dev container should be running");
-        assertTrue(prodApp.isRunning(), "Prod container should be running");
-
-        System.out.println("Dev container ID: " + devApp.getContainerId());
-        System.out.println("Prod container ID: " + prodApp.getContainerId());
-        System.out.println("Dev mapped port: " + devApp.getMappedPort(8080));
-        System.out.println("Prod mapped port: " + prodApp.getMappedPort(8081));
     }
 }
